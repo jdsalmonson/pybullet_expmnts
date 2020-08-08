a0f8f301cb82e7ee6756d6271915063e84ba822a
Jay D. Salmonson <jdsalmonson@gmail.com>
2020-08-08 15:31:51 -0700

    plot legend based on first plot; more robust

--- plot_log.py
+++ plot_log.py
@@ -5,12 +5,12 @@ import pylab as plt
 
 from robotlog import RobotLog
 
-'''
-log_file_name = "LOG_IK_0001.txt"
+log_file_name = "LOG00010.txt" #"LOG_IK_0001.txt"
 botNum = 1
 '''
 log_file_name = "LOG_IK_PANDA_0001.txt"
 botNum = 7
+'''
 
 bot_log = RobotLog(log_file_name)
 
@@ -59,13 +59,27 @@ for iplt in range(numplots):
 
     acc = np.clip(acc, -200, 200)
     #stpz = 0.5 * (stp[:-1] + stp[1:])
-    axs[rw, cl].plot(tmstmp, log['q'+str(iplt)], label = "q")
-    axs[rw, cl].plot(tmstmp, log['u'+str(iplt)], label = "u")
-    axs[rw, cl].plot(tmstmp, un, label = "u smooth")
-    axs[rw, cl].plot(tmstmpz, dq, label = "dq")
-    axs[rw, cl].plot(tmstmpz, acc / 10.,'--', label = "a/10")
+    pla = []
+    pla.extend( axs[rw, cl].plot(tmstmp, log['q'+str(iplt)], label = "q") )
+    pla.extend( axs[rw, cl].plot(tmstmp, log['u'+str(iplt)], label = "u") )
+    #pla.extend( axs[rw, cl].plot(tmstmp, un, label = "u smooth") )
+    #pla.extend( axs[rw, cl].plot(tmstmpz, dq, label = "dq") )
+    pla.extend( axs[rw, cl].plot(tmstmpz, acc / 10.,'--', label = "a/10") )
+    torque_key = 't'+str(iplt)
+    if torque_key in log.keys():
+        #axs[rw, cl].plot(tmstmp, smooth(np.array(log[torque_key]), window_len=2)/10, label = "torque/10")
+        #pla.extend( axs[rw, cl].plot(tmstmp, smooth(np.array(log[torque_key]), window_len=11), label = "smooth torque") )
+        pla.extend( axs[rw, cl].plot(tmstmp, np.array(log[torque_key]), label = "torque") )
+
+    # assemble list of plots & legends from first plot,
+    # otherwise sometimes matplotlib doesn't get the plot legends
+    if iplt == 0:
+        pl_leg = []
+        for pl in pla:
+            pl_leg.append([pl, pl.get_label()])
+
     axs[rw, cl].set_title(f"Joint {iplt}")
 
-plt.legend()
+plt.legend(*zip(*pl_leg))
 #plt.tight_layout()
 plt.show()
